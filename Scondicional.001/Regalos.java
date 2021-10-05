823fdf4d252f330fbca8fc1c86ad606818f025bc
cristhianlaura0 <>
2021-10-05 19:28:19 +0000

    lapices

---
+++ Scondicional.001/Regalos.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+
+public class Regalos{
+
+static scanner sc = new scanner(system.in);
+
+static void algoritmoregalo()
+
+public static void main(String[] arg){
+
+} 
+
+}
---
+++ cpl.lv/calprofinal.py
@@ -0,0 +1,13 @@
+print("calcular promedio final")
+#datos de entrada
+nota1=float(input("ingrese nota 1:"))
+nota1=float(input("ingrese nota 2:"))
+nota1=float(input("ingrese nota 3:"))
+nota1=float(input("ingrese nota 4:"))
+nota1=float(input("ingrese nota 5:"))
+nota1=float(input("ingrese nota 6:"))
+nota1=float(input("ingrese nota 7:"))
+#proceso
+promediofin=(nota1*0.15)+(nota2*0.15)+(nota3*0.15)+(nota4*0.10)+(nota5*0.5)+(nota6*0.10)+(nota7*0.30)
+#datos de salida
+print(f"su promedio final es: (round(promediofin .2))
