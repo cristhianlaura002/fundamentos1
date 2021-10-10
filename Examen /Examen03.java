34792ed3a971a4c343456c54e79582ccbb031877
cristhianlaura0 <>
2021-10-10 04:28:10 +0000

    examen01

---
+++ Examen /Examen03.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BC {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int edad, sexo;
+        System.out.print("Ingresa el valor de edad: ");
+        edad = in.nextInt();
+        in.nextLine();
+        System.out.println("Selecciona el valor de sexo.");
+        System.out.println("\t1.");
+        do {
+            sexo = in.nextInt();
+            in.nextLine();
+            if (sexo<1||sexo>2)
+                System.out.print("Valor incorrecto. Ingr\u00E9salo nuevamente.: ");
+        } while (sexo<1||sexo>2);
+        if((sexo==2&&edad>=16&&edad<70)||edad<16)
+            System.out.println("A");
+        if(sexo==1&&edad>=16&&edad<70)
+            System.out.println("B");
+        if(edad>70)
+            System.out.println("C");
+    }
+
+} 
--- Scondicional.001/Regalos.java
+++ Scondicional.001/Regalos.java
@@ -1,13 +1,25 @@
-import java.util.scanner;
-
+import java.util.Scanner; 
 public class Regalos{
-
-static scanner sc = new scanner(system.in);
-
-static void algoritmoregalo()
-
-public static void main(String[] arg){
-
-} 
-
+static Scanner sc = new Scanner(System.in);
+static void algoritmoRegalos(){
+  //Definir Variables
+  double monto=0;
+  String regalo="";
+  //Datos de entrada
+  System.out.println("Ingrese el monto que dispone:");  
+  monto=sc.nextDouble();
+  //Proceso
+  if(monto>=1 && monto<=10){
+    regalo="\nTarjeta";    
+  } else if(monto>=11 && monto<=100){
+    regalo="\nChocolate\nTarjeta";
+  }else if(monto>=101 && monto<=250){
+    regalo="\nFlores\nChocolate\nTarjeta";
+  }else if(monto>250){
+    regalo="\nAnillo\nFlores\nChocolate\nTarjeta";
+  }else{
+    regalo="Ninguno";
+  }
+  //Datos de salida
+  System.out.println("Ud. puede elegir los siguientes: "+ regalo);
 }
